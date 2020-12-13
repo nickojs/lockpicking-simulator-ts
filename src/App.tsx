eeb8a168a98387a08872245e49f27ed332b29c93
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-13 06:05:57 -0300

    feat(app): import routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import routes from './routes/routes';
 
 export default () => (
   <>
     <Navbar />
+    {routes}
   </>
 );
