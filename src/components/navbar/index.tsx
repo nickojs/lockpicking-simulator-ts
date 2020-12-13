4275f0d9264574bc8cf5f465488618e3865818d4
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-13 06:40:58 -0300

    fix(navbar): adjust styles and component's behavior

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,23 +4,25 @@ import arrow from '../../assets/sideMenuArrow.png';
 
 export default () => {
   // const dispatch = useDispatch();
-  const [toggleMenu, setToggleMenu] = useState(true);
+  const [menu, setMenu] = useState(false);
   // const { isAuth, username } = useSelector((state) => state.user);
 
+  const isAuth = false;
+  const username = 'false';
+
   const returnButtonHandler = () => {
-    setToggleMenu(!toggleMenu);
+    setMenu(false);
     // dispatch({ type: 'RESET_GAME' });
   };
 
   return (
-    <S.Header toggle={toggleMenu}>
+    <S.Header menu={menu}>
       <S.Navbar>
-        <h1>Not ss</h1>
-        {/* <h1>{isAuth ? username : 'Not Logged In'}  </h1> */}
+        <h1>{isAuth ? username : 'Not Logged In'}</h1>
         <S.Button to="/" onClick={returnButtonHandler}>Home</S.Button>
       </S.Navbar>
-      <S.ArrowContainer toggle={toggleMenu}>
-        <S.Arrow src={arrow} alt="Arrow" onClick={() => setToggleMenu(!toggleMenu)} />
+      <S.ArrowContainer menu={menu}>
+        <S.Arrow src={arrow} alt="Arrow" onClick={() => setMenu(!menu)} />
       </S.ArrowContainer>
     </S.Header>
   );
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -2,13 +2,13 @@ import styled, { keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 interface StyledProps {
-  toggle: boolean;
+  menu: boolean;
 }
 
 export const Header = styled.div`
   position: fixed;
   left: 0; right: 0;
-  ${(props: StyledProps) => (props.toggle ? 'top: -46px' : 'top: 0px')}
+  ${({ menu }: StyledProps) => (menu ? 'top: 0px' : 'top: -50px')};
   z-index: 20;
   transition: top .5s;
 `;
@@ -17,6 +17,7 @@ export const Navbar = styled.nav`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   
   padding: 12px;
   box-sizing: border-box;
@@ -39,7 +40,7 @@ export const ArrowContainer = styled.div`
   height: 28px;
   width: 28px;
   transition: transform .5s;
-  transform: ${({ toggle }: StyledProps) => !toggle && 'rotate(180deg)'};
+  transform: ${({ menu }: StyledProps) => menu && 'rotate(180deg)'};
 `;
 
 // using translateX because the image is flipped
