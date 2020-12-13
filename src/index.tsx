483d37ff6bda25604d44f9999a20b40ed65e5eeb
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-13 06:43:28 -0300

    feat(style and fonts): add WebfontLoader lib and index.css from previous project

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import WebFont from 'webfontloader';
 import App from './App';
+
+WebFont.load({
+  google: {
+    families: ['Cinzel:400,700', 'Cormorant Garamond:400', 'serif']
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
