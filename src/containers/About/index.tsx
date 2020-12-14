89bc86a7aaef7d50be9b532a982d66e5aaf08d87
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-13 22:18:12 -0300

    feat(about): add about page

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as S from './styles';
+import { Title, Line, InnerContainer } from '../../generalStyles';
+
+export default () => (
+  <S.Container>
+    <S.LayerContainer>
+      <S.LimitContainer>
+        <InnerContainer>
+          <Title>About</Title>
+        </InnerContainer>
+        <Line>
+          Coded by:
+          {' '}
+          <S.Link href="https://github.com/nickojs">This dude</S.Link>
+        </Line>
+        <Line>
+          Developed with ReactJs, using
+          {' '}
+          <S.Link href="https://www.npmjcom/package/cra-template-nickojs">my clean template</S.Link>
+          , check it out!
+        </Line>
+        <Line>Backgrounds are mostly Skyrim screenshots found on the web</Line>
+        <Line>Menu arrows created by my beautiful brother</Line>
+        <Line>Lockpick vector, among with other vectors created by myself.</Line>
+        <Line>
+          The angle calculations needed for the useAngle hook were based on
+          {' '}
+          <br />
+          <S.Link href="https://stackoverflow.com/questions/15653801/rotating-object-to-face-mouse-pointer-on-mousemove">
+            this stackoverflow answer
+          </S.Link>
+          .
+        </Line>
+        <Line>
+          The custom cursor is available
+          {' '}
+          <br />
+          <S.Link href="http://www.rw-designer.com/cursor-detail/30682">here</S.Link>
+        </Line>
+      </S.LimitContainer>
+    </S.LayerContainer>
+  </S.Container>
+);
---
+++ src/containers/About/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { Container as C, Link as L } from '../../generalStyles';
+
+import background from '../../assets/image/about.jpg';
+
+export const Container = styled(C)`
+  text-align: center;
+  background-image: url(${background});
+`;
+
+export const LayerContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  `;
+
+export const LimitContainer = styled.div`
+  width: 60%;
+  margin: 0 auto;
+`;
+
+export const Link = styled(L)`
+  color: gold;
+  &:hover{ text-shadow: 1px 1px 10px white; }
+  &:visited{ color: gold; }
+`;
