f8887b2ce99b48745f22221732992e36237ffcce
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-14 08:02:52 -0300

    feat(redux): create base for a redux implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import WebFont from 'webfontloader';
-import App from './App';
+import store from './store/store';
 
 WebFont.load({
   google: {
@@ -11,11 +12,24 @@ WebFont.load({
   }
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  // eslint-disable-next-line global-require
+  const App = require('./App').default;
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', render);
+}
